8d6f67c87c17fc9e082989d671c6fc9169aed54d
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-05-04 13:40:20 +0700

    Register user and staff models in admin

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserAccount, Staff
 
-# Register your models here.
+
+@admin.register(UserAccount)
+class UserAccountAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Security Fields", {
+            "fields": (
+                "mfaEnabled",
+                "failedLoginAttempts",
+                "lockedUntil",
+            )
+        }),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "is_active",
+        "mfaEnabled",
+        "failedLoginAttempts",
+        "lockedUntil",
+    )
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "role")
+    search_fields = ("name", "user__username")
+    list_filter = ("role",)
